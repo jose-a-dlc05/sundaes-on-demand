378bbab1fcf97d89cb43b2b31fd413bdd678fb89
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-09-01 20:30:31 -0400

    Added popover test

--- src/pages/summary/tests/SummaryForm.test.jsx
+++ src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,7 +1,12 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
-describe('Checkbox', () => {
+describe('SummaryForm', () => {
 	test('checkbox is unchecked by default', () => {
 		render(<SummaryForm />);
 		const checkbox = screen.getByRole('checkbox', {
@@ -16,7 +21,7 @@ describe('Checkbox', () => {
 			name: /terms and conditions/i,
 		});
 		const button = screen.getByRole('button', { name: 'Confirm order' });
-		fireEvent.click(checkbox);
+		userEvent.click(checkbox);
 		expect(checkbox).toBeChecked();
 		expect(button).toBeEnabled();
 	});
@@ -27,11 +32,35 @@ describe('Checkbox', () => {
 			name: /terms and conditions/i,
 		});
 		const button = screen.getByRole('button', { name: 'Confirm order' });
-		fireEvent.click(checkbox);
+		userEvent.click(checkbox);
 		expect(checkbox).toBeChecked();
 		expect(button).toBeEnabled();
-		fireEvent.click(checkbox);
+		userEvent.click(checkbox);
 		expect(checkbox).not.toBeChecked();
 		expect(button).toBeDisabled();
+	});
+
+	test('popover responds to hover', async () => {
+		render(<SummaryForm />);
+		// popover starts out hidden
+		const nullPopover = screen.queryByText(
+			/no ice cream will actually be delivered/i
+		);
+		expect(nullPopover).not.toBeInTheDocument();
+
+		// popover appears upon mouseover of checkbox label
+		const termsAndConditions = screen.getByText(/terms and conditions/i);
+		userEvent.hover(termsAndConditions);
+
+		const popover = screen.getByText(
+			/no ice cream will actually be delivered/i
+		);
+		expect(popover).toBeInTheDocument();
+
+		// popover disappears when we mouse out
+		userEvent.unhover(termsAndConditions);
+		await waitForElementToBeRemoved(() =>
+			screen.queryByText(/no ice cream will actually be delivered/i)
+		);
 	});
 });
